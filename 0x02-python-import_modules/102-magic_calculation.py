8ad8f0db41ed63d1fbaffd95fe7b5201e9154e42
Mbukiseni <mbndima@gmail.com>
2023-09-07 02:37:31 -0700

    102-magic_calculation.py

--- 0x02-python-import_modules/102-magic_calculation.py
+++ 0x02-python-import_modules/102-magic_calculation.py
@@ -2,13 +2,17 @@
 
 def magic_calculation(a, b):
     """Match bytecode provided by Holberton School."""
-    from magic_calculation_102 import add, sub
+    try:
+        from magic_calculation_102 import add, sub
+    except ImportError:
+        return None  # Handle the case where the module cannot be imported
 
     if a < b:
         c = add(a, b)
         for i in range(4, 6):
             c = add(c, i)
-        return (c)
+        return c  # Return c without parentheses
 
     else:
-        return(sub(a, b))
+        return sub(a, b)
+
