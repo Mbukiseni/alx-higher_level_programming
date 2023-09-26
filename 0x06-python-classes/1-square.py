58f005c5ab020fcec0969f3fe08303961c2a6de6
Mbukiseni <mbndima@gmail.com>
2023-09-26 05:04:41 -0700

    1-square.py

---
+++ 0x06-python-classes/1-square.py
@@ -0,0 +1,14 @@
+#!/usr/bin/python3
+"""Define the class Square."""
+
+
+class Square:
+    """Represent th square."""
+
+    def __init__(self, size):
+        """Initialize the new Square.
+
+        Args:
+            size (int): The size of new square.
+        """
+        self.__size = size
