9e98cba0aaaed1289e218569bdf99113f90990e3
Mbukiseni <mbndima@gmail.com>
2023-10-09 05:00:22 -0700

    0-lookup.py

--- 0x0A-python-inheritance/0-lookup.py
+++ 0x0A-python-inheritance/0-lookup.py
@@ -1,5 +1,6 @@
 #!/usr/bin/python3
-"""Defines available methods of the object."""
+"""Defines available attributes/methods of an object."""
+
 
 def lookup(obj):
     """Return a list of available attributes."""
